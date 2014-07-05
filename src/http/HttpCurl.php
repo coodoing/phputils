650d25069b873e0d2a20b22ebeeb806f567ce50a
coodoing <jairdxs@gmail.com>
2014-07-05 10:35:20 +0800

    curl

--- src/http/HttpCurl.php
+++ src/http/HttpCurl.php
@@ -33,11 +33,10 @@ class PU_HttpCurl{
 		$this->_curlOptions [$name] = $value;
 	}
 
-	public function setCredentials($username, $password) {
-		$this->addHeader ( 'Authorization', 'Basic ' . base64_encode ( $username . ':' . $password ) );
-	}
-
-	function setHeaders($headers) {
+	/**
+	 * define a set of HTTP headers to be sent to the server
+	 **/
+	public function setHeaders($headers) {
 		if (is_array ( $headers )) {
 			foreach ( $headers as $name => $value ) {
 				$this->addHeader ( $name, $value );
@@ -45,24 +44,20 @@ class PU_HttpCurl{
 		}
 	}
 
-	public function addHeader($headerName, $headerValue = null) {
-		$lower_name = strtolower ( $headerName );
-		
+	protected function addHeader($headerName, $headerValue = null) {
+		$lower_name = strtolower ( $headerName );		
 		// Check if $name needs to be split
 		if ($headerValue === null && (strpos ( $headerName, ':' ) > 0)) {
 			list ( $headerName, $headerValue ) = explode ( ':', $headerName, 2 );
-		}
-		
+		}		
 		// Make sure the name is valid
 		if (! preg_match ( '/^[a-zA-Z0-9-]+$/', $headerName )) {
 			return false;
-		}
-		
+		}		
 		// If $value is null or false, unset the header
 		if ($headerValue === null || $headerValue === false) {
 			unset ( $this->_requestHeaders [$lower_name] );
-			return false;
-		
+			return false;		
 		// Else, set the header
 		} else {
 			// Header names are stored lowercase internally.
@@ -71,8 +66,7 @@ class PU_HttpCurl{
 			}
 			$this->_requestHeaders [$lower_name] = array ($headerName, $headerValue );
 		}
-		return true;
-	
+		return true;	
 	}
 
 	public function removeHeader($headerName) {
@@ -82,6 +76,7 @@ class PU_HttpCurl{
 
 	public function addCookies($cookiesArr){
 		if (is_array($cookiesArr)){
+			// be cautious about the use of array_merge
 			$this->_requestCookies = array_merge($this->_requestCookies,$cookiesArr);	
 		}
 	}	
@@ -89,114 +84,98 @@ class PU_HttpCurl{
 	public function doGet($url, $query_params = '') {
 		if (! empty ( $query_params ) && is_array ( $query_params )) {
 			$url .= (strpos($url,'?')===false? '?' : '&') . http_build_query ( $query_params, null, '&' );
-			}
-
-        return $this->_request ( self::HTTP_METHOD_GET, $url );
+		}
+        return $this->curl ( self::HTTP_METHOD_GET, $url );
 	}
 
 	public function doPost($url, $postVars) {
-		return $this->_request ( self::HTTP_METHOD_POST, $url, $postVars );
+		return $this->curl ( self::HTTP_METHOD_POST, $url, $postVars );
 	}
 
 	public function doPut($url, $putVars=null) {
-		return $this->_request ( self::HTTP_METHOD_PUT, $url, $putVars );
+		return $this->curl ( self::HTTP_METHOD_PUT, $url, $putVars );
 	}
 	
 	public function doDelete($url, $query_params = '') {
 		if (! empty ( $query_params ) && is_array ( $query_params )) {
             $url .= (strpos($url,'?')===false? '?' : '&') . http_build_query ( $query_params, null, '&' );
 		}
-
-		return $this->_request ( self::HTTP_METHOD_DELETE, $url );
+		return $this->curl ( self::HTTP_METHOD_DELETE, $url );
 	}
 
 	public function doHead($url) {
-		return $this->_request ( self::HTTP_METHOD_HEAD, $url );
+		return $this->curl ( self::HTTP_METHOD_HEAD, $url );
 	}
 
-	final private function _request($method, $url, $postargs = array()) {
+	/*
+	 * do the http request curl
+	 * normal procedure of the curl mehotd:
+	 * 1 $ch = curl_init($url)
+	 * 2 curl_exec($ch)
+	 * 3 curl_close($ch)
+	*/
+	final private function curl($method, $url, $postargs = array()) {
 	    if(isset($_GET['curl_debug'.CSS_JS_VERSION.'_start']) && $_GET['curl_debug'.CSS_JS_VERSION.'_start'] == 100){
 	        list($usec, $sec) = explode(" ", microtime());
 		    $timedebug1 = ((float)$usec + (float)$sec);	
 	    }
-		// Get the curl session object
-		$session = curl_init ( $url );
-		
-		//设置CURL Options
+		// Get the curl ch_session object
+		$ch_session = curl_init ( $url );		
 		foreach ( $this->_curlOptions as $name => $value ) {
-			curl_setopt ( $session, $name, $value );
-		}
-		
+			curl_setopt ( $ch_session, $name, $value );
+		}		
 		//set http header options
 		if (! empty ( $this->_requestHeaders ) && is_array ( $this->_requestHeaders )) {
-			$headers = $this->_prepareHeaders ();
-			curl_setopt ( $session, CURLOPT_HTTPHEADER, $headers );
+			$headers = $this->preProcessHeaders ();
+			curl_setopt ( $ch_session, CURLOPT_HTTPHEADER, $headers );
 		}
 		//set cookies to header
 		if (!empty($this->_requestCookies)){
-			$cookies = $this->_prepareCookies();
-			curl_setopt($session, CURLOPT_COOKIE, $cookies);
+			$cookies = $this->preProcessCookies();
+			curl_setopt($ch_session, CURLOPT_COOKIE, $cookies);
 		}
 		if ($method == self::HTTP_METHOD_POST) {
-			curl_setopt ( $session, CURLOPT_POST, 1 );
-			curl_setopt ( $session, CURLOPT_POSTFIELDS, $postargs );
+			curl_setopt ( $ch_session, CURLOPT_POST, 1 );
+			curl_setopt ( $ch_session, CURLOPT_POSTFIELDS, $postargs );
 		}
 		elseif ($method == self::HTTP_METHOD_PUT) {
-            curl_setopt ( $session, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt ( $ch_session, CURLOPT_CUSTOMREQUEST, 'PUT');
             $headers[] = 'Content-Length: ' . strlen($postargs);
-            curl_setopt ( $session, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt ( $session, CURLOPT_POSTFIELDS, $postargs );
+            curl_setopt ( $ch_session, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt ( $ch_session, CURLOPT_POSTFIELDS, $postargs );
 		}
 		elseif ($method == self::HTTP_METHOD_DELETE) {
-            curl_setopt ( $session, CURLOPT_CUSTOMREQUEST, 'DELETE');
+			// A custom request method to use instead of "GET" or "HEAD" when doing a HTTP request
+            curl_setopt ( $ch_session, CURLOPT_CUSTOMREQUEST, 'DELETE');
 		}
-		//PAYPAL忽略ssl验证
-		curl_setopt($session, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($session, CURLOPT_SSL_VERIFYHOST, 2);
         
-        //
-        //curl_setopt($session, CURLOPT_HTTPHEADER, array('Expect:')); 
-		// Do the curl and then close the session
-		$response = curl_exec ( $session );
-		$curl_info = curl_getinfo ( $session );
+		// Do the curl and then close the ch_session
+		$response = curl_exec ( $ch_session );
+		$curl_info = curl_getinfo ( $ch_session );
 		
 		$httpResp = null;
-		if (curl_errno ( $session )) {
-			$err_msg = curl_error ( $session );
-			litb_log_error ( "Curl URL: {$url} with {$method} error :{$err_msg}" );
+		if (curl_errno ( $ch_session )) {
+			$err_msg = curl_error ( $ch_session );
 		} else {
-			curl_close ( $session );
-			$httpResp = velaHttpResponse::fromString ( $response );
+			curl_close ( $ch_session );
+			$httpResp = ( $response );
 		}
-		//如果请求rul里带上开启curl调试参数则发邮件
-		if(isset($_GET['curl_debug'.CSS_JS_VERSION.'_start']) && $_GET['curl_debug'.CSS_JS_VERSION.'_start'] == 100){
-            list($usec, $sec) = explode(" ", microtime());
-		    $timedebug2 = ((float)$usec + (float)$sec);
-		    litb_log_error ( array('logKey'=>'curl debug','desc'=>'curl debug','curl url'=>$url,'request url'=>$_SERVER['REQUEST_URI'],'method'=> $method, 'cost time'=>$timedebug2 - $timedebug1));
-        }
 		return $httpResp;
 	}
 
-	private function _prepareHeaders() {
-		
-		$headers = array ();
-		
-		// Add all other user defined headers
+	protected function preProcessHeaders() {		
+		$headers = array ();		
 		foreach ( $this->_requestHeaders as $header ) {
 			list ( $name, $value ) = $header;
 			if (is_array ( $value )) {
 				$value = implode ( ', ', $value );
-			}
-			
+			}			
 			$headers [] = "$name: $value";
-		}
-		
-		return $headers;
-	
+		}		
+		return $headers;	
 	}
 
-	public function _prepareCookies(){
-		//
+	protected function preProcessCookies(){
 		$cookies = array();
 		if (is_array($this->_requestCookies)){
 			foreach($this->_requestCookies as $key => $value){
