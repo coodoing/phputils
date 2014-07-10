056e1b07de6761a61b95a7392c54468578896de7
coodoing <jairdxs@gmail.com>
2014-07-10 23:29:38 +0800

    rsa

---
+++ src/security/RSA.php
@@ -0,0 +1,36 @@
+<?php
+
+class PU_RSA extends PU_SecurityAbstract{
+
+	public function __construct(){
+
+	}	
+
+	public function encryption ($plaintext){
+
+	}
+
+    public function decryption ($ciphertext){
+
+    }
+
+    public function getEncryptionKey (){
+
+    }
+
+    public function getDecryptionKey (){
+
+    }
+
+    public function getEncryptionType (){
+
+    }
+ 
+    public function getLastError (){
+
+    }
+
+    public function getMaxLenCrypted (){
+
+    }
+}
